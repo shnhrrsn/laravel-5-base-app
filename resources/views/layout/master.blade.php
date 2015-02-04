23d2d3d62b75d3c6197017fd51e2eb272435a88c
Shaun Harrison <shnhrrsn@gmail.com>
2015-02-04 15:08:03 -0500

    Removed hard coded repo for shnhrrsn/laravel-assets and now using the official one via packagist

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		{!! HTML::style('http://fonts.googleapis.com/css?family=Lato:100') !!}
-		{!! HTML::style(HTML::assetPath('scss/welcome.scss')) !!}
+		{!! HTML::style(asset_path('scss/welcome.scss')) !!}
 	</head>
 	<body>
 		<div class="container">
