0389c00b0eaaa9644164339a210d5245523c3fb0
Shaun Harrison <shnhrrsn@gmail.com>
2015-02-04 13:49:43 -0500

    Removed default traits from BaseController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,5 @@
+<?php namespace App\Http\Controllers;
+
+abstract class BaseController extends \Illuminate\Routing\Controller {
+
+}
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,6 +1,6 @@
 <?php namespace App\Http\Controllers;
 
-class WelcomeController extends Controller {
+class WelcomeController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
